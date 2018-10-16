a1be132683900fc9666f79fefb49c8accfdfca68
GuangWong <guangwong@gmail.com>
2018-10-16 16:12:34 +0800

    chore: update cov.sh

--- scripts/cov.sh
+++ scripts/cov.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
-lerna run cov
 cwd=`pwd`
-if [ -d "${cwd}/.nyc_output" ]; then
-  rm -rf "${cwd}/.nyc_output"
-fi
+lerna run cov
+rm -rf "${cwd}/.nyc_output" || true
 mkdir "${cwd}/.nyc_output"
 cp -r ./packages/*/.nyc_output/* $cwd/.nyc_output/ || true
 cp -r ./packages/*/node_modules/.nyc_output/* $cwd/.nyc_output/ || true
 ./node_modules/.bin/nyc report
-
