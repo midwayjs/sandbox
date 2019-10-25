d6565228607bd56d6746f43572b1f9e95797901a
Mario Du <dujiakun@gmail.com>
2019-10-25 15:17:45 +0800

    fix: fix types

--- packages/sandbox-core/src/core/service/remoteDebugService.ts
+++ packages/sandbox-core/src/core/service/remoteDebugService.ts
@@ -8,7 +8,7 @@ import {IRemoteDebugService} from '../../interface/services/IRemoteDebugService'
 export class RemoteDebugService implements IRemoteDebugService {
 
   @inject('pandoraAdapter')
-  protected pandoraAdapter: PandoraAdapter;
+  pandoraAdapter: PandoraAdapter;
 
   async getDebuggableHost(options: HostSelector & AppSelector & UserSelector): Promise<DebuggableHost> {
     const {uid} = options;
