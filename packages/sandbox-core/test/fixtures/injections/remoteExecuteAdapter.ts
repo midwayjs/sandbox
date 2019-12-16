ae82d7424136a357c74b29dad1cb99733b32301b
Chunlin-Li <zhglclyx@163.com>
2019-12-16 11:55:58 +0800

    fix: remove query param

--- packages/sandbox-core/test/fixtures/injections/remoteExecuteAdapter.ts
+++ packages/sandbox-core/test/fixtures/injections/remoteExecuteAdapter.ts
@@ -6,7 +6,7 @@ export class RemoteExecuteAdapter {
   async exec(host: HostSelector, cmd): Promise<string> {
     if (cmd.match(/.*\/json\/list.*/)) {
       return JSON.stringify([{ id: 'd1ba2672-5121-4fd5-969e-78bbb1f64a47' }]);
-    } else if (cmd.match(/.*\/process\?appName.*/)) {
+    } else if (cmd.match(/.*\/process.*/)) {
       return JSON.stringify({
         data: [
           { debugPort: 3001 },
